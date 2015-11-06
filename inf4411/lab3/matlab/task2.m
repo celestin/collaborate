1241bc2c22de45e2fb286e88b1693575b5b71a4d
Vegard Midtbøen <vegard.midtboen@outlook.com>
2015-11-06 12:14:21 +0100

    cadence

---
+++ inf4411/lab3/matlab/task2.m
@@ -0,0 +1,30 @@
+%% Task2
+
+clear all;
+close all;
+clc;
+
+load('cadence_plot_task2b');
+load('task2_phase');
+
+x = aaa(:,1);
+y = aaa(:,2);
+x_p = phase(:,1);
+y_p = phase(:,2);
+
+f1 = figure;
+f1.Position = [500, 500, 840, 630];
+
+subplot(2,1,1);
+loglog(x, y);
+grid on;
+title('Plot of magnitude')
+xlabel('Freq, [Hz]')
+ylabel('Magnitude, [V]');
+
+subplot(2,1,2);
+semilogx(x_p, y_p);
+grid on; 
+title('Phase plot')
+xlabel('Freq, [Hz]');
+ylabel('Angle, [Deg]');
