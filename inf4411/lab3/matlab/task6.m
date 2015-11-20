caeee89d4895d610231393e55082c681c66b21ad
Vegard Midtbøen <vegard.midtboen@outlook.com>
2015-11-20 15:43:48 +0100

    La til figurer og opprettet task6 og task6_take2

--- inf4411/lab3/matlab/task6.m
+++ inf4411/lab3/matlab/task6.m
@@ -10,8 +10,7 @@ addpath(genpath('/hom/mes/src/matlab/gpib/linux'));
 amps = 10e-3;
 
 v_dd = 10; % port +25V
-%v_bias = 3.8; % port +6V
-num_samples = 20;
+num_samples = 100;
 
 % init equiment voltage supply
 HPE3631_Init;
@@ -22,45 +21,34 @@ HPE3631_SetILimit (2, amps);
 HP34401_Init;
 HP34401_SetMode('VD');
 
-% init equiment function generator
-HP33120_Init;
-HP33120_SetFreq(100e3); % Set a frewuency
-
-
 HPE3631_SetVolt (2, v_dd); % Uses port 2 (+25V) as v_dd
 
-v = linspace (3.9 , 4.2 , num_samples);
+v = linspace (3.76 , 3.82, num_samples);
 
 im = zeros(1, size(v, 2)); 
-
-HPE3631_SetVolt (2, v_dd); % Uses port 2 (+25V) as v_dd
-%HPE3631_Operate();
 
 % Do experiment
 pause(1);
 for i = 1:size(v, 2)
+%     f = figure(i);
     
-    %HPE3631_SetVolt(1, v(1, i));
     HPE3631_SetVolt(1, v(1, i)); % Uses port 1 (+6V) as the sweep voltage
-   
-   HP34401_ReadQuick();
-    
-    % Do a read to get instrument to change resolution
-%     K617_ReadQuick();
+
+    HP34401_ReadQuick();
+
     %let it settle
-    pause(0.05); 
+    pause(0.5); 
+    
     % sample and store
-%     im(1, i) = K617_ReadQuick();
- im(1, i) = HP34401_ReadQuick();
+    im(1, i) = HP34401_ReadQuick();
+
 end;
 
 % Turn off voltage
 HPE3631_SetVolt (1, 0);
 HPE3631_SetVolt (2, 0);
 HPE3631_SetVolt (3, 0);
-%HPE3631_Disable ();
 
-% [time, data] = HP54622_GetData(HP54622_DefaultAdr);
 %% Plot results
 s = 200;
 f1 = figure;
@@ -70,7 +58,7 @@ figure(f1)
 plot(v, im, 'LineWidth', 2);
 grid on;
 
-title('Task 6, V_{nzero} vs Vout');
+title('Task 6, Finding DC point');
 xlabel('V_{nzero}, V');
 ylabel('Vout, V');
 
